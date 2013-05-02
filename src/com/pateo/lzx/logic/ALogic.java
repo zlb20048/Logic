04fdaf97257f06c605de84f687044cc0161bd54f
zixiangliu <zixiangliu@pateo.com.cn>
2013-05-02 15:45:35 +0800

    init logic project
    init logic project

---
+++ src/com/pateo/lzx/logic/ALogic.java
@@ -0,0 +1,27 @@
+package com.pateo.lzx.logic;
+
+public class ALogic
+{
+    public static void main(String[] args)
+    {
+        ALogic logic = new ALogic();
+        System.out.println(logic.getTotal(3));
+    }
+
+    public int getTotal(int number)
+    {
+        if (number == 1)
+        {
+            return 1;
+        }
+        else if (number == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return number + getTotal(number - 1);
+        }
+    }
+
+}
